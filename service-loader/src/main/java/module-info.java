71bb8d407e6c23a0de67ba26a0e1adb3ca61e4d7
rbradley813 <rbradley813@gmail.com>
2024-10-02 10:35:54 -0400

    initial service-loader commit

--- service-loader/src/main/java/module-info.java
+++ service-loader/src/main/java/module-info.java
@@ -1,2 +1,3 @@
 module dev.ikm.commons.service.loader {
+    exports dev.ikm.commons.service.loader;
 }
--- service-loader/src/test/java/dev/ikm/commons/service/loader/IKMServiceLoaderTest.java
+++ service-loader/src/test/java/dev/ikm/commons/service/loader/IKMServiceLoaderTest.java
@@ -1,4 +1,4 @@
 package dev.ikm.commons.service.loader;
 
-public class DefaultServiceLoader {
+public class IKMServiceLoaderTest {
 }
