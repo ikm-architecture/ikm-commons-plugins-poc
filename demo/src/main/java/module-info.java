e8920f3b2300fd7c6ab2d12a5a84eb9cd866e87a
rbradley813 <rbradley813@gmail.com>
2024-11-01 17:04:20 -0400

    update demo code, pom, and module-info

--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -1,9 +1,14 @@
 module dev.ikm.commons.demo {
     requires dev.ikm.plugins.api;
     requires dev.ikm.plugins.identicon.api;
+    requires dev.ikm.plugin.service.loader;
+    requires dev.ikm.tinkar.common;
+    requires dev.ikm.tinkar.provider.executor;
+    requires dev.ikm.tinkar.plugin.service.boot;
 
     exports dev.ikm.commons.demo;
 
     uses dev.ikm.plugins.api.Plugin;
     uses dev.ikm.plugins.identicon.api.IdenticonPlugin;
+    uses dev.ikm.tinkar.common.service.PluginServiceLoader;
 }
